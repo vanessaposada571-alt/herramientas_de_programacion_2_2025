b2892dc29c5d96a735ff66c9131d810e490c79da
Daniimazo <daniel60mv@gmail.com>
2025-09-10 18:30:21 -0500

    add file services and ports

--- Hospital/obj/Debug/net8.0-windows/Hospital.AssemblyInfo.cs
+++ Hospital/obj/Debug/net8.0-windows/Hospital.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb2b927e8a295e58feee5dc4c5a7e8380766ff7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+544c0f87eba207459cb780231ce05f75886074cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
